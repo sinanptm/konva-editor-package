8a59415d80bf76acce521b326c156e4f1387a873
muhammed sinan <muhammedsinan7749@gmail.com>
2025-07-11 17:47:36 +0530

    Update package version to 1.0.1, refactor exports in index files, and ensure consistent default export for ImageEditor component.

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,4 +2,4 @@ import * as react_jsx_runtime from 'react/jsx-runtime';
 
 declare const ImageEditor: () => react_jsx_runtime.JSX.Element;
 
-export { ImageEditor };
+export { ImageEditor as default };
